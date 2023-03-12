9f0143eb8960f91514e5d89963328fb90eaf5bd8
Hruday Vairagade <hruday.vairagade97@gmail.com>
2023-03-12 11:32:51 -0400

    Final Changes

--- src/components/VoiceNotes.js
+++ src/components/VoiceNotes.js
@@ -1,8 +1,21 @@
 import { useWhisper } from '@chengsokdara/use-whisper'
-
 import React from 'react'
 
 export default function VoiceNotes() {
+    const url = "http://35.153.51.197/voice-note"
+    const postData = (text)=>{
+      fetch(url, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body:JSON.stringify({
+              input: text
+          })
+      }).then(res=>res.json())
+      .then(data=>console.log(data))
+      .catch(err=>console.log(err))
+  }
     const {
         recording,
         speaking,
@@ -12,10 +25,12 @@ export default function VoiceNotes() {
         startRecording,
         stopRecording,
       } = useWhisper({
-        apiKey: "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",
+        apiKey: process.env.OPENAIKEY,
         streaming: true,
         timeSlice: 1_000
       })
+
+      
     
       return (
         <div>
@@ -26,6 +41,7 @@ export default function VoiceNotes() {
           <button onClick={() => startRecording()}>Start</button>
           <button onClick={() => pauseRecording()}>Pause</button>
           <button onClick={() => stopRecording()}>Stop</button>
+          <button onClick={() => postData(transcript.text)}>Post</button>
         </div>
       )
 }
